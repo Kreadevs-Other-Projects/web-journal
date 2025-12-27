9be54ad7dad0db287f65b0c8c5b310dc2938dcb0
saadkhan2211 <saadkhan.og01@gmail.com>
2025-12-27 21:39:04 +0500

    refactor: streamline JWT token generation and update environment variable types

--- server/src/configs/envs.ts
+++ server/src/configs/envs.ts
@@ -1,12 +1,16 @@
 import "dotenv/config";
+import type { StringValue } from "ms";
 
 export const env = {
+  NODE_ENV: process.env.NODE_ENV || "development",
   PORT: process.env.PORT,
   DATABASE_URL: process.env.DATABASE_URL as string,
   SALT_ROUND: parseInt(process.env.SALT_ROUND!),
   JWT_SECRET: process.env.JWT_SECRET as string,
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
-  JWT_SHORT_EXPIRY: process.env.JWT_SHORT_EXPIRY as string,
-  JWT_LONG_EXPIRY: process.env.JWT_LONG_EXPIRY as string,
+
+  JWT_SHORT_EXPIRY: process.env.JWT_SHORT_EXPIRY as StringValue,
+  JWT_LONG_EXPIRY: process.env.JWT_LONG_EXPIRY as StringValue,
+
   CORS_ORIGIN: process.env.CORS_ORIGIN,
 };
--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -2,11 +2,7 @@ import jwt from "jsonwebtoken";
 import { env } from "../configs/envs";
 
 export const generateAccessToken = async (userId: string) => {
-  return jwt.sign(
-    {
-      userId,
-    },
-    env.JWT_SECRET,
-    { expiresIn: (env.JWT_SHORT_EXPIRY as string) || "24h" }
-  );
+  return jwt.sign({ userId }, env.JWT_SECRET, {
+    expiresIn: env.JWT_SHORT_EXPIRY,
+  });
 };

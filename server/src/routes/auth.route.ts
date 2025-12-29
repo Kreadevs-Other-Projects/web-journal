5e6d7e50e8156eeadf1e0591f65f11cc621aa02e
saadkhan2211 <saadkhan.og01@gmail.com>
2025-12-29 10:09:42 +0500

    added validation middleware and auth schema

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { asyncHandler } from "../utils/asyncHandler";
 import { login, signup } from "../controllers/auth.controller";
+import { validate } from "../middlewares/validate.middleware";
+import { loginSchema, signupSchema } from "../schemas/auth.schema";
 
 const router = Router();
 
-router.post("/login", asyncHandler(login));
-router.post("/signup", asyncHandler(signup));
+router.post("/login", validate(loginSchema), asyncHandler(login));
+router.post("/signup", validate(signupSchema), asyncHandler(signup));
 
 export default router;

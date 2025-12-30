f86c1b3582162e5a6599950e547ecec3828ebbbf
Arqam Souban <arqamsouban55@gmail.com>
2025-12-30 18:34:39 +0500

    refactor: simplify validation middleware and improve error handling

--- server/src/middlewares/validate.middleware.ts
+++ server/src/middlewares/validate.middleware.ts
@@ -1,32 +1,16 @@
+import { ZodObject } from "zod";
 import { Request, Response, NextFunction } from "express";
-import { z, ZodError } from "zod";
 
-export const validate = (schema: z.ZodSchema) => {
-  return (req: Request, res: Response, next: NextFunction) => {
+export const validate =
+  (schema: ZodObject) => (req: Request, res: Response, next: NextFunction) => {
     try {
-      console.log("Request body:", req.body);
       schema.parse(req.body);
       next();
-    } catch (error) {
-      console.log("Validation error:", error);
-
-      if (error instanceof ZodError) {
-        const errors = error.issues.map((err) => ({
-          field: err.path.join("."),
-          message: err.message,
-        }));
-
-        return res.status(400).json({
-          success: false,
-          message: "Validation error",
-          errors: errors,
-        });
-      }
-
+    } catch (error: any) {
       return res.status(400).json({
         success: false,
         message: "Validation error",
+        errors: error.errors,
       });
     }
   };
-};

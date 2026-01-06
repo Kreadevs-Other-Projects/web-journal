f9b683c716de82f28ad8dd95413c7e7759f7ca50
Arqam Souban <arqamsouban55@gmail.com>
2026-01-06 19:19:15 +0500

    fix

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,5 +1,13 @@
 import { Request, Response } from "express";
 import {
+  findUserByEmail,
+  findUserById,
+  hashPassword,
+  createUser,
+  validatePassword,
+  createUserProfile,
+} from "../services/auth.service";
+import {
   createOTP,
   verifyOTP,
   checkOTPVerified,
